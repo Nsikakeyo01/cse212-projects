a199882f35e6880f182edcf1905202ba60a565c5
Nsikak Eyo <mrnsikakeyo@gmail.com>
2026-01-20 14:11:26 +0100

    update

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -1,69 +1,52 @@
-/// <summary>
-/// Defines a maze using a dictionary. The dictionary is provided by the
-/// user when the Maze object is created. The dictionary will contain the
-/// following mapping:
-///
-/// (x,y) : [left, right, up, down]
-///
-/// 'x' and 'y' are integers and represents locations in the maze.
-/// 'left', 'right', 'up', and 'down' are boolean are represent valid directions
-///
-/// If a direction is false, then we can assume there is a wall in that direction.
-/// If a direction is true, then we can proceed.  
-///
-/// If there is a wall, then throw an InvalidOperationException with the message "Can't go that way!".  If there is no wall,
-/// then the 'currX' and 'currY' values should be changed.
-/// </summary>
+// Maze.cs
+using System;
+using System.Collections.Generic;
+
 public class Maze
 {
-    private readonly Dictionary<ValueTuple<int, int>, bool[]> _mazeMap;
-    private int _currX = 1;
-    private int _currY = 1;
+    private readonly Dictionary<(int, int), bool[]> maze;
+    private int x = 1;
+    private int y = 1;
 
-    public Maze(Dictionary<ValueTuple<int, int>, bool[]> mazeMap)
+    public Maze(Dictionary<(int, int), bool[]> map)
     {
-        _mazeMap = mazeMap;
-    }
-
-    // TODO Problem 4 - ADD YOUR CODE HERE
-    /// <summary>
-    /// Check to see if you can move left.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveLeft()
-    {
-        // FILL IN CODE
-    }
-
-    /// <summary>
-    /// Check to see if you can move right.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveRight()
-    {
-        // FILL IN CODE
-    }
-
-    /// <summary>
-    /// Check to see if you can move up.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveUp()
-    {
-        // FILL IN CODE
-    }
-
-    /// <summary>
-    /// Check to see if you can move down.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
-    public void MoveDown()
-    {
-        // FILL IN CODE
+        maze = map;
     }
 
     public string GetStatus()
     {
-        return $"Current location (x={_currX}, y={_currY})";
+        return $"Current location (x={x}, y={y})";
+    }
+
+    public void MoveLeft()
+    {
+        if (!maze[(x, y)][0])
+            throw new InvalidOperationException("Can't go that way!");
+
+        x--;
+    }
+
+    public void MoveRight()
+    {
+        if (!maze[(x, y)][1])
+            throw new InvalidOperationException("Can't go that way!");
+
+        x++;
+    }
+
+    public void MoveUp()
+    {
+        if (!maze[(x, y)][2])
+            throw new InvalidOperationException("Can't go that way!");
+
+        y--; // moving up decreases y
+    }
+
+    public void MoveDown()
+    {
+        if (!maze[(x, y)][3])
+            throw new InvalidOperationException("Can't go that way!");
+
+        y++; // moving down increases y
     }
 }
